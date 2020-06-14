809f6c236710240efdb41731d166941ec521b961
Aravinth Panch <me@aravinth.info>
2020-06-14 14:20:46 +0200

    Update meta tag

--- src/index/sections/meta.php
+++ src/index/sections/meta.php
@@ -3,20 +3,20 @@
 <meta name="description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
 
 <!-- Google / Search Engine Tags -->
-<meta itemprop="name" content="Aravinth Panch">
+<meta itemprop="name" content="Aravinth Panch | Interdisciplinary Expert">
 <meta itemprop="description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
 <meta itemprop="image" content="assets/images/portfolio/ara-banner.jpg">
 
 <!-- Facebook Meta Tags -->
 <meta property="og:url" content="https://aravinth.info">
 <meta property="og:type" content="website">
-<meta property="og:title" content="Aravinth Panch">
+<meta property="og:title" content="Aravinth Panch | Interdisciplinary Expert">
 <meta property="og:description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
 <meta property="og:image" content="assets/images/portfolio/ara-banner.jpg">
 
 <!-- Twitter Meta Tags -->
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="Aravinth Panch">
+<meta name="twitter:title" content="Aravinth Panch | Interdisciplinary Expert">
 <meta name="twitter:description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
 <meta name="twitter:image" content="assets/images/portfolio/ara-banner.jpg">
 
