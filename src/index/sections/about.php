d0901e796a475cb50a2bfd91976c64de0e59c25a
Aravinth Panch <me@aravinth.info>
2020-06-17 11:11:54 +0200

    Update about

--- src/index/sections/about.php
+++ src/index/sections/about.php
@@ -17,19 +17,19 @@
               Aravinth is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability, with the core expertise in research, development and production of electro-mechanical embedded systems and digital products.
             </p>
             <p class="text-justify">
+              As an engineer, he worked for 2 corporates in the telecom sector and worked for 8 tech startups in the sector of education, smart home, media, agriculture, energy, healthcare & water, in Asia & Europe, where one of the hardware product was sold in 30K+ quantities and software product reached 10M+ users.
+            </p>
+            <p class="text-justify">
               As an entrepreneur, he co-founded 7 organisations in the sector of education, sustainability, agriculture, consulting & engineering services, in Asia & Europe.
-            </p>
-            <p class="text-justify">
-              As an engineer, he worked for 2 corporates in the telecom sector and worked for 8 tech startups in the sector of education, smart home, media, agriculture, energy, healthcare & water, in Asia & Europe.
-            </p>
-            <p class="text-justify">
-              As an educator, he trained several young innovators and mentored 120+ deep tech startups in the domain of advanced materials, healthcare, agriculture, aerospace, energy, automotive & biotechnology, globally.
             </p>
             <p class="text-justify">
               As a scientist, he researched at 4 academic research institutions in the field of wireless technologies, autonomous vehicles, biometrics, humanoid robotics & machine learning, in Germany.
             </p>
             <p class="text-justify">
-              As an advisor, he supported several individuals, startups, accelerators, incubators, innovation hubs, corporates, investors, manufacturers, research institutions, universities, government initiatives, art galleries and social enterprises, globally.
+              As an educator, he trained several young innovators and mentored 120+ deep tech startups in the domain of advanced materials, healthcare, agriculture, aerospace, energy, automotive & biotechnology, globally.
+            </p>
+            <p class="text-justify">
+              As an advisor, he is affiliated with 23 organisations and supported several individuals, startups, accelerators, incubators, innovation hubs, corporates, investors, manufacturers, research institutions, universities, government initiatives, art galleries and social enterprises, in more than 50 countries.
             </p>
             <p class="text-justify">
               As an artist, he has co-created several new media art installations for temporary and permanent exhibitions, which attracted more than 1.5 million visitors, in Germany, Australia & USA.
