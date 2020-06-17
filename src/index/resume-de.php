b9c8e8c05928128055ba1ac794ec127907b291b5
Aravinth Panch <me@aravinth.info>
2020-06-17 11:30:25 +0200

    Add about section in German

---
+++ src/index/resume-de.php
@@ -0,0 +1,3 @@
+  <?php include('0-top.php');  ?>
+  <?php include('sections/about-de.php');  ?>
+  <?php include('1-bottom.php');  ?>
---
+++ src/index/sections/about-de.php
@@ -0,0 +1,80 @@
+<!-- About Section Begin -->
+<section class="about" id="about">
+  <div class="title section-title text-center">
+    <div class="title-icon-container">
+      <div class="title-icon"><i class="pe-7s-id pe-2x"></i></div>
+    </div>
+    <h1>Aravinth Panch</h1>
+    <h2>Interdisziplinärer Experte</h2>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="description">
+          <div class="text">
+            <p></p>
+            <p class="text-justify">
+              Aravinth ist ein interdisziplinärer Experte mit mehr als 17 Jahren globaler Erfahrung in Wissenschaft, Technik, Kunst und Nachhaltigkeit. Er verfügt über die Kernkompetenz in Forschung, Entwicklung und Produktion von elektromechanischen eingebetteten Systemen und digitalen Produkten.
+            </p>
+            <p class="text-justify">
+              Als Ingenieur, arbeitete er für 2 große Unternehmen im Telekommunikationssektor und für 8 Tech-Startups im Bereich Bildung, Smart Home, Medien, Landwirtschaft, Energie, Gesundheitswesen und Wasser, in Asien und Europa, wo eines der Hardwareprodukte in mehr als 30.000 Stück verkauft wurde und das Softwareprodukt mehr als 10 Millionen Benutzer erreichte.
+            </p>
+            <p class="text-justify">
+              Als Unternehmer, war er Mitbegründer von 7 Organisationen in den Bereichen Bildung, Nachhaltigkeit, Landwirtschaft, Beratung und Ingenieurdienstleistungen, in Asien und Europa.
+            </p>
+            <p class="text-justify">
+              Als Wissenschaftler, forschte er an 4 akademischen Forschungseinrichtungen im Bereich Drahtlose Technologien, Autonome Fahrzeuge, Biometrie, Humanoide Robotik und Maschinelles Lernen, in Deutschland.
+            </p>
+            <p class="text-justify">
+              Als Mentor, bildete er mehrere junge Innovatoren aus und betreute mehr als 120 Deep-Tech-Startups in den Bereichen Fortschrittliche Materialien, Gesundheitswesen, Landwirtschaft, Luft- und Raumfahrt, Energie, Automobilindustrie und Biotechnologie, weltweit.
+            </p>
+            <p class="text-justify">
+              Als Berater, ist er Mitglied von 23 Organisationen und unterstützte mehrere Einzelpersonen, Startups, Gründerzentren, Innovationszentren, Unternehmen, Investoren, Hersteller, Forschungseinrichtungen, Universitäten, Regierungsinitiativen, Kunstgalerien und Sozialunternehmen, in mehr als 50 Ländern.
+            </p>
+            <p class="text-justify">
+              Als Künstler, hat er mehrere Kunstinstallationen für neue Medien für temporäre und permanente Ausstellungen mitgestaltet, die mehr als 1,5 Millionen Besucher, in Deutschland, Australien und den USA anzogen.
+            </p>
+            <p class="text-justify">
+              Als Community-Builder, hat er mehrere Communities für Startups, Nachhaltigkeit, Bildung und Kunst mitgestaltet und kuratiert, in Asien, Europa und Afrika.
+            </p>
+            </br>
+            <!-- <h3 style="color: #f9bf3b;">Public Appearances</h3> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+  <div class="container">
+    <div class="row">
+      <div class="col-6 col-md-4"></div>
+      <div class="col-6 col-md-4" style="text-align: center;">
+        <h5>
+          <a href="me@aravinth.info" data-toggle="tooltip" title="GitHub">
+            <div class="circle">
+              <i class="fa fa-envelope"></i>
+            </div>
+          </a>
+          <a href="www.aravinth.info" data-toggle="tooltip" title="GitHub">
+            <div class="circle">
+              <i class="fa fa-external-link"></i>
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/aravinthpanch" data-toggle="tooltip" title="Linkedin">
+            <div class="circle">
+              <i class="fa fa-linkedin"></i>
+            </div>
+          </a>
+          <a href="https://github.com/aravinthpanch" data-toggle="tooltip" title="GitHub">
+            <div class="circle">
+              <i class="fa fa-github"></i>
+            </div>
+          </a>
+
+        </h5>
+      </div>
+      <div class="col-6 col-md-4"></div>
+    </div>
+  </div>
+</section>
+<!-- About section End -->
