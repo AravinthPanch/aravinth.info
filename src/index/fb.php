28d639a7ba8c422240516db08f9fd0b1c21c455d
Aravinth Panch <me@aravinth.info>
2020-08-18 15:41:05 +0200

    Update FB meta

--- src/index/fb.php
+++ src/index/fb.php
@@ -1,23 +1,23 @@
 <!-- HTML Meta Tags -->
 <title>Aravinth Panch</title>
-<meta name="description" content="Aravinth | Scientist">
+<meta name="description" content="Aravinth is a Sri Lankan innovator">
 
 <!-- Google / Search Engine Tags -->
-<meta itemprop="name" content="Follow my page to become a Sri Lankan innovator">
-<meta itemprop="description" content="Aravinth | Scientist">
+<meta itemprop="name" content="Follow my page to become the next Sri Lankan innovator">
+<meta itemprop="description" content="Aravinth is a Sri Lankan innovator">
 <meta itemprop="image" content="assets/images/portfolio/ara-banner.jpg">
 
 <!-- Facebook Meta Tags -->
 <meta property="og:url" content="https://www.facebook.com/ara.panch">
 <meta property="og:type" content="website">
-<meta property="og:title" content="Follow my page to become a Sri Lankan innovator">
-<meta property="og:description" content="Aravinth | Scientist">
+<meta property="og:title" content="Follow my page to become the next Sri Lankan innovator">
+<meta property="og:description" content="Aravinth is a Sri Lankan innovator">
 <meta property="og:image" content="assets/images/portfolio/ara-banner.jpg">
 
 <!-- Twitter Meta Tags -->
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="Follow my page to become a Sri Lankan innovator">
-<meta name="twitter:description" content="Aravinth | Scientist">
+<meta name="twitter:title" content="Follow my page to become the next Sri Lankan innovator">
+<meta name="twitter:description" content="Aravinth is a Sri Lankan innovator">
 <meta name="twitter:image" content="assets/images/portfolio/ara-banner.jpg">
 
 <!-- Meta Tags Generated via http://heymeta.com -->
