a453436c164182d0a54b4e5d0cd6c40c43fd413b
Aravinth Panch <me@aravinth.info>
2020-08-18 15:48:05 +0200

    Add index with meta for FB

---
+++ src/index/0-top-fb.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include('sections/meta-fb.php');  ?>
+
+  <!-- Stylesheets -->
+  <!-- Bootstrap -->
+  <link href="assets/stylesheets/css/bootstrap/bootstrap.css" rel="stylesheet">
+  <!-- font-awesome.css -->
+  <link href="assets/stylesheets/css/fontawesome/font-awesome.css" rel="stylesheet">
+  <!-- pe-icon.css -->
+  <link href="assets/stylesheets/css/pe-icon/pe-icon-7-stroke.css" rel="stylesheet">
+  <link href="assets/stylesheets/css/pe-icon/helper.css" rel="stylesheet">
+  <!-- Animate.css -->
+  <link href="assets/stylesheets/css/animate.css" rel="stylesheet">
+  <!-- Owl Carousel -->
+  <link href="assets/stylesheets/css/owl-carousel/owl.carousel.css" rel="stylesheet">
+  <link href="assets/stylesheets/css/owl-carousel/owl.theme.css" rel="stylesheet">
+  <!-- Slick -->
+  <link href="assets/stylesheets/css/slick/slick.css" rel="stylesheet">
+  <link href="assets/stylesheets/css/slick/slick-theme.css" rel="stylesheet">
+  <!-- Main CSS -->
+  <link href="assets/stylesheets/css/style.css" rel="stylesheet">
+  <!-- <link href="assets/stylesheets/css/color-scheme.css" rel="stylesheet"> -->
+  <!-- Font style live -->
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
+  <!-- Style Switcher -->
+  <link href="assets/stylesheets/css/switcher/switcher.css" rel="stylesheet">
+  <link href="assets/stylesheets/css/colors/yellow.css" id="switch_style" rel="stylesheet" />
+
+  <?php include('sections/cookie-consent.php');  ?>
+</head>
+
+<body data-spy="scroll" data-target=".navbar-collapse" data-offset="80" class="slideshowbody">
