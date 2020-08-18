cfdf3cb9acda36e07e93db48bce9039a00c0d828
Aravinth Panch <me@aravinth.info>
2020-08-18 15:29:15 +0200

    Add FB meta

---
+++ src/index/fb.php
@@ -0,0 +1,23 @@
+<!-- HTML Meta Tags -->
+<title>Aravinth Panch</title>
+<meta name="description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
+
+<!-- Google / Search Engine Tags -->
+<meta itemprop="name" content="Follow my page to become an innovator">
+<meta itemprop="description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
+<meta itemprop="image" content="assets/images/portfolio/ara-banner.jpg">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://www.facebook.com/ara.panch">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Follow my page to become an innovator">
+<meta property="og:description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
+<meta property="og:image" content="assets/images/portfolio/ara-banner.jpg">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Follow my page to become an innovator">
+<meta name="twitter:description" content="Aravinth Panch is an interdisciplinary expert with 17+ years of global experience in science, engineering, arts and sustainability.">
+<meta name="twitter:image" content="assets/images/portfolio/ara-banner.jpg">
+
+<!-- Meta Tags Generated via http://heymeta.com -->
