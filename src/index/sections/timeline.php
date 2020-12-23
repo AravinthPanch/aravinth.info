bc4588ee50ff2d52f469b64bd06fee6238c525c1
Aravinth Panch <me@aravinth.info>
2020-12-23 23:08:32 +0100

    Update timeline size

--- src/index/sections/timeline.php
+++ src/index/sections/timeline.php
@@ -2,7 +2,7 @@
 <section>
   <div class="container">
     <div class="row">
-      <div class="col-md-8 col-md-offset-2">
+      <div class="col-md-12">
         <div class="description">
           <div class="text">
             <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1euk116wcC2-S8o1JCg8taIwoWNCU8S2mOR_2L7HqPxY&font=Default&lang=en&start_at_end=true&initial_zoom=0&height=650' width='100%' height='650'
